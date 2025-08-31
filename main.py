33061dee78aef46760d84ec9aaded425c334ca76
PedroRevesMD <pedroreves11@gmail.com>
2025-08-31 14:32:48 -0300

    feat: can parse course titles and websites

--- main.py
+++ main.py
@@ -55,27 +55,6 @@ def select_course_info(html: BeautifulSoup) -> List[CourseInfo]:
     return courses
 
 
-def generate_course_url(title: str, base_url="https://www.cps.sp.gov.br/etec") -> str:
-    normalized_title = normalize_course_name(title)
-    return f"{base_url}/{normalized_title}"
-
-
-def normalize_course_name(course_name: str) -> str:
-    normalized = course_name.lower()
-    normalized = unicodedata.normalize("NFD", normalized)
-    normalized = "".join(
-        char for char in normalized if unicodedata.category(char) != "Mn"
-    )
-
-    normalized = re.sub(r"[^\w\s-]", "", normalized)
-    normalized = re.sub(r"\s+", " ", normalized).strip()
-    normalized = normalized.replace(" ", "-")
-    normalized = re.sub(r"-+", "-", normalized)
-    normalized = normalized.strip("-")
-
-    return normalized
-
-
 def main():
     page = make_requests(
         "https://www.cps.sp.gov.br/etec/cursos-oferecidos-pelas-etecs/"

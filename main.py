c5855692119607620eb9fba9a4a0c40981fcacea
PedroRevesMD <pedroreves11@gmail.com>
2025-08-31 14:31:32 -0300

    feat: can parse title and modalities from website

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import regex as re
+import unicodedata
 from dataclasses import dataclass
 from typing import List
 from bs4 import BeautifulSoup
@@ -32,6 +34,46 @@ def parse_page(html_content: str) -> BeautifulSoup:
         raise ValueError(f"Cannot parse website. Try Again! {e}") from e
 
     return content
+
+
+def select_course_info(html: BeautifulSoup) -> List[CourseInfo]:
+    courses: List[CourseInfo] = []
+
+    course_page_elements = html.find_all("div", class_="listagem-posts-conteudos")
+
+    for element in course_page_elements:
+        course_title: str = (
+            element.find("h3", class_="listagem-posts-titulo").text.strip() or ""
+        )
+        course_modalities = element.find_all("span", class_="term-lista-tipo")
+        modalities = []
+        for modality in course_modalities:
+            text = modality.text.strip()
+            modalities.append(text)
+        course_url = generate_course_url(course_title)
+
+    return courses
+
+
+def generate_course_url(title: str, base_url="https://www.cps.sp.gov.br/etec") -> str:
+    normalized_title = normalize_course_name(title)
+    return f"{base_url}/{normalized_title}"
+
+
+def normalize_course_name(course_name: str) -> str:
+    normalized = course_name.lower()
+    normalized = unicodedata.normalize("NFD", normalized)
+    normalized = "".join(
+        char for char in normalized if unicodedata.category(char) != "Mn"
+    )
+
+    normalized = re.sub(r"[^\w\s-]", "", normalized)
+    normalized = re.sub(r"\s+", " ", normalized).strip()
+    normalized = normalized.replace(" ", "-")
+    normalized = re.sub(r"-+", "-", normalized)
+    normalized = normalized.strip("-")
+
+    return normalized
 
 
 def main():

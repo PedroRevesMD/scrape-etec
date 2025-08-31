e43201df1c1a16304bb99c8179f96c72acc31552
PedroRevesMD <pedroreves11@gmail.com>
2025-08-31 11:41:28 -0300

    feat: parsing websites

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ from bs4 import BeautifulSoup
 def make_requests(url: str, timeout: int = 3):
     page = requests.get(url, timeout=timeout)
     page.raise_for_status()
-    return page
+    return page.text
+
+
+def parse_page(page: str) -> BeautifulSoup:
+    content = BeautifulSoup(page, "html.parser")
+    print(content.prettify())
+    return content
 
 
 def main():
-    make_requests("https://www.cps.sp.gov.br/etec/cursos-oferecidos-pelas-etecs/")
+    page = make_requests(
+        "https://www.cps.sp.gov.br/etec/cursos-oferecidos-pelas-etecs/"
+    )
+    content = parse_page(page)
+    print(content)
 
 
 if __name__ == "__main__":
